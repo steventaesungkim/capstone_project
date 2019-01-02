af3f43a30b1fbf77a37264ad8c26f5702cbd7905
Steven Taesung Kim <steventaesungkim@gmail.com>
2019-01-02 09:59:39 -0500

    intial commit for capstone with database

---
+++ schema.sql
@@ -0,0 +1,38 @@
+CREATE TABLE users(
+    id serial primary key,
+    userName text,
+    passWord text,
+    name text,
+    avatar text
+);
+
+CREATE TABLE categories(
+    id serial primary key,
+    category_type text,
+    level boolean,
+    theuser_id integer references users (id)
+);
+
+CREATE TABLE timers(
+    id serial primary key,
+    time timestamp,
+    level text,
+    category_id integer references categories (id),
+    theuser_id integer references users (id)
+);
+
+CREATE TABLE questions(
+    id serial primary key,
+    level text,
+    question text,
+    answer text,
+    category_id integer references categories (id)
+);
+
+CREATE TABLE results(
+    id serial primary key,
+    correct boolean,
+    time timestamp,
+    theuser_id integer references users (id),
+    question_id integer references questions (id)
+);
---
+++ seed.sql
@@ -0,0 +1,30 @@
+INSERT INTO users
+    (userName, passWord, name, avatar)
+VALUES
+    ('stan', 'stan', 'stan', 'stanIMG'),
+    ('greg', 'greg', 'greg', 'gregIMG'),
+    ('kim', 'kim', 'kim', 'kimIMG');
+
+INSERT INTO categories
+    (category_type, level, theuser_id)
+VALUES
+    ('arithemic', true, 1),
+    ('sequence', true, 2);
+
+INSERT INTO timers
+    (time, level, category_id, theuser_id)
+VALUES
+    ('Jan 2 2019 09:33:34', '1', 1, 1),
+    ('Jan 3 2019 09:12:35', '2', 2, 2);
+
+INSERT INTO questions
+    (level, question, answer, category_id)
+VALUES
+    ('1', '1+1', '2', 1),
+    ('1', '1122', '33', 2);
+
+INSERT INTO results
+    (correct, time, theuser_id, question_id)
+VALUES
+    (true, 'Jan 2 2019 09:33:34', 1, 1),
+    (true, 'Jan 3 2019 09:12:35', 2, 2);

de37229b9857572e71a76f6d135acce8e2f9e930
Greg Foreman <gfore@mindspring.com>
2019-01-09 18:19:19 -0500

    consolidates CSS into index.css

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import Home from './Home';
 
-import './App.css';
-
 
 class App extends Component {
     render() {
@@ -15,19 +13,3 @@ class App extends Component {
 }
 
 export default App;
-
-
-{/* <header className="App-header">
-<img src={logo} className="App-logo" alt="logo" />
-<p>
-  Edit <code>src/App.js</code> and save to reload.
-</p>
-<a
-  className="App-link"
-  href="https://reactjs.org"
-  target="_blank"
-  rel="noopener noreferrer"
->
-  Learn React
-</a>
-</header> */}

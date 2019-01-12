e27da9c42c20a602eaacb5fa50bb1844e14f3aaa
Stanley Chen <dannyshien@gmail.com>
2019-01-12 14:01:36 -0500

    workin progress on myaccount

--- client/src/Settings.js
+++ client/src/Settings.js
@@ -5,6 +5,9 @@ const Settings = (props) => {
     return (
         <div>
             <h2>Settings</h2>
+            <p>Please update your information</p>
+
+            
         </div>
     );
 }

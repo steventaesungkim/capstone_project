0f3a59c6466c4a02a55940562225801d511ad09c
Stanley Chen <dannyshien@gmail.com>
2019-01-12 16:07:13 -0500

    process of setting up update form

--- client/src/MyAccount.js
+++ client/src/MyAccount.js
@@ -2,12 +2,23 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class MyAccount extends Component {
-    // constructor(props) {
-    //     super(props); 
-    //     this.state = {
+    constructor(props) {
+        super(props); 
+        this.state = {
+            username: '',
+            password:'',
+            // avatar: ''
+        }
+    }
 
-    //     }
-    // }
+    componentDidMount() {
+        fetch('/api/user/userId')
+        .then(r => r.json())
+        .then(data => {
+            console.log(data)
+
+        })
+    }
 
     render() {
         return (
--- client/src/Settings.js
+++ client/src/Settings.js
@@ -7,7 +7,30 @@ const Settings = (props) => {
             <h2>Settings</h2>
             <p>Please update your information</p>
 
-            
+            {/* <form>
+                <label>Username:</label>
+                <input 
+                    name='username'
+                    className='input'
+                    type='text'
+
+                />
+                <br/>
+                <label>Password:</label>
+                <input 
+                    name='password'
+                    className='input'
+                    type='text'
+
+                />
+                <br/>
+                <input 
+                    className='input-submit'
+                    type='submit'
+                    value='Update'
+                />
+
+            </form>     */}
         </div>
     );
 }

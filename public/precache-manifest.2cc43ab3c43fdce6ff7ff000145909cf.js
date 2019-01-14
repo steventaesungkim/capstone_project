a9e32859c7c7d102a194be0559bc3dcd4392add6
Stanley Chen <dannyshien@gmail.com>
2019-01-14 14:36:48 -0500

    add react build files

---
+++ public/precache-manifest.2cc43ab3c43fdce6ff7ff000145909cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dcad8ec1901906993ffa",
+    "url": "/static/css/main.a381fda0.chunk.css"
+  },
+  {
+    "revision": "dcad8ec1901906993ffa",
+    "url": "/static/js/main.dcad8ec1.chunk.js"
+  },
+  {
+    "revision": "7bff42754f284ed44d14",
+    "url": "/static/js/1.7bff4275.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4d5a35d3804b001494b82b7c4467c320",
+    "url": "/index.html"
+  }
+];
